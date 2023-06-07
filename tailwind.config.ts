e61f67172ec6ce831bd89092d505596331f724e9
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-06-07 23:00:37 +0700

    Setup UI for the application

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "app-black-soft": "#1C1A17",
+        "app-black": "#000000",
+        "app-red": "#EF1B27",
+        "app-vermillion": "#FF2B06",
+        "app-yellow": "#FECC4E",
+        "app-gray": "#D7D2D0",
+        "app-yellow-pale": "#FFDB80",
+        "app-red-pale": "#F56870",
+        "app-green": "#3FB160",
+        "app-gold": "#FEB20E",
+        "app-blue": "#046EE7",
+        "app-purple": "#7B61FF",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
