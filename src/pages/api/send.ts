1b621d5968543bcf898d2337b462118cf742f890
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-06-09 06:41:48 +0700

    Updating checking state and send api to send.ts(2)

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -1,8 +1,15 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import type { NextApiResponse } from "next";
 
-export default async function handler(req: any, res: NextApiResponse) {
-  const { message, selectedImages } = req.body;
-  const captchaImages = JSON.parse(localStorage.getItem("captchaImages") || "null");
-  console.log(captchaImages, message, selectedImages);
-  // res.json(body: {});
-}
+export default withIronSessionApiRoute(
+  async function handler(req: any, res: NextApiResponse) {
+    const { message, selectedImages } = req.body;
+    const captchaImages = req.session.captchaImages
+    console.log(captchaImages, message, selectedImages);
+    // res.json(body: {});
+  },
+  {
+    cookieName: "session",
+    password: process.env.SECRET_SESSION || "",
+  }
+);
