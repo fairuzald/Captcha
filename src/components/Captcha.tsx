094bd493c907045c5c2aa5cdcb9bdb7d13521d44
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-06-08 13:28:38 +0700

    Setup to install Iron Session and use it to make api to render image on captcha and prevent dissappear when reloading images

--- src/components/Captcha.tsx
+++ src/components/Captcha.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image";
 
 const Captcha: React.FC = () => {
-  const imagesArray = new Array(9).fill(null).map((value, index) => {
-    return `/api/captcha-image?index=${index}.png`;
+  const imagesArray = new Array(9).fill(null).map((value, index: number) => {
+    return `/api/captcha-image?index=${index}`;
   });
 
   return (
     <div className="container grid grid-cols-3">
       {imagesArray.map((imageUrl: string, index: number) => {
         return (
-          <Image
-            key={index}
-            src={imageUrl}
-            alt={`Image number ${index}`}
-            width={100}
-            height={100}
-            className="h-[100px] w-[100px] border-2 border-app-red bg-white"
-          />
+          <div key={index} className="h-[100px] w-[100px] border-2 border-app-red bg-white">
+            <Image
+              src={imageUrl}
+              alt={`Image number ${index+1}`}
+              width={100}
+              height={100}
+              className=" w-full h-full object-cover object-center"
+            />
+          </div>
         );
       })}
     </div>
