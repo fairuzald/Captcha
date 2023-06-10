90a3528026415705c106119c05fee5f7f5b417a9
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-06-10 11:55:02 +0700

    Reupdate with the old version with little addition

---
+++ components/Captcha.jsx
@@ -0,0 +1,48 @@
+import {useEffect, useState} from "react";
+import Image from 'next/link'
+
+export default function Captcha({onChange,captchaKey}) {
+  const [selectedIndexes,setSelectedIndexes] = useState([]);
+  useEffect(() => {
+    onChange(selectedIndexes);
+  }, [selectedIndexes]);
+  useEffect(() => {
+    setSelectedIndexes([]);
+  }, [captchaKey]);
+
+  const imageLocations = (new Array(9))
+    .fill(null)
+    .map((value, index) => {
+      return `/api/captcha-image?index=${index}&key=${captchaKey}`;
+    });
+  function toggleIndex(index) {
+    setSelectedIndexes(prev => {
+      if (prev.includes(index)) {
+        return prev.filter(v => v !== index);
+      } else {
+        return [...prev, index];
+      }
+    })
+  }
+  return (
+    <div className="captcha">
+      <h2>Select all dogs:</h2>
+      <div className="captcha-images">
+        {imageLocations.map((imageUrl,index) => (
+          <div
+            key={index}
+            onClick={() => toggleIndex(index)}
+            className={selectedIndexes.includes(index) ? 'selected' : ''}>
+            <Image
+              src={imageUrl}
+              alt={`Image number ${index + 1}`}
+              width={150}
+              height={150}
+              priority
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
